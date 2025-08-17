9323b7858ad389c9a6f7ef6912382d81613d3001
MinJeung-Kim <focso5@gmail.com>
2025-08-17 12:58:14 +0900

    Fix: EACCES Error

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  // 개발 서버 설정
+  server: {
+    port: 3000, // 원하는 포트
+    host: "127.0.0.1", // IPv6(::1) 대신 IPv4로 강제 (EACCES 회피)
+    strictPort: true, // 3000 사용 불가 시 자동 증가 방지
+    open: true, // 브라우저 자동 오픈 
+  },
 });
